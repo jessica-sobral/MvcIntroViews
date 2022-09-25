6e1977cc5f0fd4f2f59b0ec788d722ecc9795e7d
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-09-25 15:19:09 -0300

    add calculadora controller and operacoes in the form

---
+++ Controllers/CalculadoraController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcIntroViews.Controllers;
+
+public class CalculadoraController : Controller
+{
+    public IActionResult Operacao()
+    {
+        return View();
+    }
+
+    public IActionResult Resultado([FromForm] string operacao, [FromForm] double n1, [FromForm] double n2)
+    {
+        ViewBag.N1 = n1;
+        ViewBag.N2 = n2;
+        ViewBag.Operacao = operacao;
+        
+        switch (operacao)
+        {
+            case "Soma":
+                ViewBag.Sinal = "+";
+                ViewBag.Resultado = n1 + n2;
+                break;
+            case "Subtração":
+                ViewBag.Sinal = "-";
+                ViewBag.Resultado = n1 - n2;
+                break;
+            case "Multiplicação":
+                ViewBag.Sinal = "*";
+                ViewBag.Resultado = n1 * n2;
+                break;
+            case "Divisão":
+                ViewBag.Sinal = "/";
+                ViewBag.Resultado = n1 / n2;
+                break;
+        }
+
+        return View();
+    }
+}
